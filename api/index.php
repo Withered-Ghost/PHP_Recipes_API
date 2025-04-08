82b98264051c585ea737c1ce9086067f95eea11a
Withered-Ghost <62442.smit@gmail.com>
2025-04-08 12:02:50 +0530

    Update index.php, directory tree

--- api/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-$conn = mysqli_connect("mysql", "root", "1123581321", "recipes_db");
---
+++ src/app/config/env_loader.php
@@ -0,0 +1,5 @@
+<?php
+putenv("db_hostname=mysql");
+putenv("db_user=root");
+putenv("db_password=1123581321");
+putenv("db_name=recipes_db");
