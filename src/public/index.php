37a2b1e44b468768a2a8ee6f78f01c0c1c774edb
Withered-Ghost <62442.smit@gmail.com>
2025-04-10 00:43:27 +0530

    Add composer service to docker-compose.yml

--- src/public/index.php
+++ src/public/index.php
@@ -2,11 +2,19 @@
 require __DIR__ . "/../app/controller/RecipeController.php";
 require __DIR__ . "/../app/config/DatabaseConnector.php";
 
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT', 'DELETE'])) {
+    if (stripos($_SERVER["CONTENT_TYPE"] ?? '', 'application/json') === false) {
+        http_response_code(400);
+        echo json_encode(["error" => "Only application/json content type is accepted."]);
+        exit();
+    }
+}
 
 $pdo = (new DatabaseConnector())->get_connector();
 
@@ -14,14 +22,8 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
 if ($uri[1] === "recipes") {
-    $uid = null;
-    $rating = null;
-    if (sizeof($uri) > 2 && $uri[2]) {
-        $uid = $uri[2];
-    }
-    if (sizeof($uri) > 3 && $uri[3]) {
-        $rating = $uri[3];
-    }
+    $uid = $uri[2] ?? null;
+    $rating = $uri[3] ?? null;
 
     $recipe_controller = new RecipeController($uid, $rating, $pdo);
     $recipe_controller->process_request();
